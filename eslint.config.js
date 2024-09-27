13ea8413e8d304837e2bfaaf6528cd73e19022bd
William Killerud <william.killerud@schibsted.com>
2024-09-27 13:15:07 +0200

    chore: use shared eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,3 @@
-import js from '@eslint/js';
-import prettierConfig from 'eslint-config-prettier';
-import prettierPlugin from 'eslint-plugin-prettier';
-import globals from 'globals';
+import config from '@podium/eslint-config';
 
-export default [
-	js.configs.recommended,
-	prettierConfig,
-	{
-		plugins: {
-			prettier: prettierPlugin,
-		},
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node,
-			},
-		},
-	},
-];
+export default config;
